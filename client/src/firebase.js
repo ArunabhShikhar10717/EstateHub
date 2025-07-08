9fbff22d5ae6ca66f8eb8ccff5fafb0c64b0c7c0
arunabhshikhar <arunabhshikhar11@gmail.com>
2025-07-09 01:27:20 +0530

    OAuth done

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,13 +4,23 @@ import { initializeApp } from 'firebase/app';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: 'mern-estate.firebaseapp.com',
+//   projectId: 'mern-estate',
+//   storageBucket: 'mern-estate.appspot.com',
+//   messagingSenderId: '1078482850952',
+//   appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+// };
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  apiKey: "AIzaSyCF4vWB_wm2Ent0jazVlZhFcWxbjYGLGow",
+  authDomain: "estatehub-8398a.firebaseapp.com",
+  projectId: "estatehub-8398a",
+  storageBucket: "estatehub-8398a.firebasestorage.app",
+  messagingSenderId: "879076181274",
+  appId: "1:879076181274:web:4ea35e111ac2f3c6f1d8a3",
+  measurementId: "G-58V49QV2BZ"
 };
 
 // Initialize Firebase
